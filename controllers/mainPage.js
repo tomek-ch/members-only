12b0576973312dd24ec9e07f2bf3c34c8d0ce5f0
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-01-30 21:27:05 +0100

    Sort messages

--- controllers/mainPage.js
+++ controllers/mainPage.js
@@ -1,6 +1,10 @@
 const Message = require('../models/Message');
 
 module.exports = async function (req, res, next) {
-    const messages = await Message.find();
+    const messages = await Message
+        .find()
+        .populate('author')
+        .sort([['timestamp', -1]])
+        .catch(next);
     res.render('index', { user: req.user, messages });
 };
