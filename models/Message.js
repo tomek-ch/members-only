167eb07761df2e649aa3d6e304512ace50c43147
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-01-30 22:08:29 +0100

    Add time to posts

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,17 @@
 const { Schema, model } = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Message = new Schema({
     title: { type: String, required: true },
-    timestamp: { type: String, required: true },
+    timestamp: { type: Number, required: true },
     text: { type: String, required: true },
     author: { type: Schema.Types.ObjectId, ref: 'User' },
 });
+
+Message
+  .virtual('time')
+  .get(function () {
+    return DateTime.fromMillis(this.timestamp).toLocaleString(DateTime.DATE_MED);
+  });
 
 module.exports = model('Message', Message);
